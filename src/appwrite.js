6533d74266eda6c2a26896937a0e6c9b1eeddedd
Okwe David Ukwuru <118400292+boikwechi@users.noreply.github.com>
2025-06-15 22:56:53 +0100

    appwrite endpoint

--- src/appwrite.js
+++ src/appwrite.js
@@ -4,7 +4,7 @@ const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID ;
 import { Client, Databases, ID, Query } from 'appwrite';
 
 const client = new Client()
-  .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
+  .setEndpoint(`https://fra.cloud.appwrite.io/v1`)
   .setProject(PROJECT_ID);
 
 const databases = new Databases(client);
